2888d3fc76a4e57e983cfe3c3dce4d50cba9da01
Godari <srinivas.godari@accenture.com>
2019-10-30 11:06:12 +0530

    test code by user

---
+++ src/main/java/Browsers99/Srini99/App.java
@@ -0,0 +1,13 @@
+package Browsers99.Srini99;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
